cbdcd42bb8f0ca56290bed32a210b23e5b398eae
Naji-Ullah <najiu1836@gmail.com>
2026-03-20 03:59:03 +0500

    My-account commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-black">
       <h1 className="text-4xl font-bold text-green-900">
-        Tailwind is working 🚀
+        Tailwind is working
       </h1>
     </div>
   );
