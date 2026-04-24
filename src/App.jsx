0d17611196b7f86a0684e860b80688ac12dd625c
Naji-Ullah <najiu1836@gmail.com>
2026-04-24 11:55:12 +0500

    fix: remove TargetCursor component from App

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,11 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import ScrollProgress from "./components/ScrollProgress";
-import TargetCursor from "./components/TargetCursor";
 import "./styles/portfolio.css";
 
 export default function App() {
   return (
     <>
-      <TargetCursor 
-        spinDuration={2}
-        hideDefaultCursor
-        parallaxOn
-        hoverDuration={0.2}
-      />
       <ScrollProgress />
       <Nav />
       <main>
